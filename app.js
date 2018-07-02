ab220b8c87f524d083b139337a9734d7cad1fd24
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-07-02 18:25:33 -0400

    added loading screen functionality

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResult);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    //hide previous results
+    document.getElementById('results').style.display = 'none';
+    //show loader
+    document.getElementById('loading').style.display = 'block';
 
-function calculateResult(e) {
+    setTimeout(calculateResult, 1000);
+    e.preventDefault();
+});
+
+function calculateResult() {
     console.log("calculating");
-    
+
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
-    
+
     const monthlyPayment = document.getElementById('monthly-payment');
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
-    
+
     const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value) / 100 / 12;
     const calculatedPayments = parseFloat(years.value) * 12;
-    
+
     //calculate monthly payment
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
     const monthly = (principal * x * calculatedInterest) / (x - 1);
-    
+
     if (isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     } else {
         console.log("Error");
         showError('Please check your input');
-        
+
     }
-    e.preventDefault();
+    // e.preventDefault();
 }
 
 function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     const errorDiv = document.createElement('div');
     errorDiv.className = 'alert alert-danger';
     errorDiv.appendChild(document.createTextNode(error));
